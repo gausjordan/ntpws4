95cb2eaa0074c7e239f5ae0519b01a749d8f9e2f
Armando <afilipi@gmail.com>
2021-12-01 04:24:39 +0100

    Bugfixes, role rade.

--- news.php
+++ news.php
@@ -4,6 +4,11 @@
 	define('__Dinamo__', TRUE);
 	include("dbconn.php");
 
+	if ($_SESSION['user']['role'] == 1)
+		$izbornik = "Admin's menu";
+	else
+		$izbornik = "Editor's menu";
+
 	if ( isset($_POST['_publishtoggle_']) ) {
 		$query = "UPDATE news SET approved = 'Y' WHERE id = " . $_POST['idvijesti'];
 		$result = @mysqli_query($MySQL, $query);
@@ -49,29 +54,39 @@
 					<p>' . $r['summary'] . '...
 					<a href="index.php?menu=12&newsid=' . $r['id'] . '">More</a></p>
 					<p>Source: ' . $r['source']. '</p>
-				</div>
-				<div class="flex-item">
-					<h3>Admin menu</h3>
-					
-					<form action="index.php?menu=2" id="publishingtoggle" name="publishingtoggle" method="POST">
-						<input type="hidden" id="' . $identifikator . '" name="' . $identifikator . '" value="TRUE">
-						<input type="hidden" id="idvijesti" name="idvijesti" value="' . $r['id'] . '">
-						<input type="submit" value="' . $tekst . '"><br>
-					</form>
+				</div>';
+				
+				if (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] != 3) {
+				echo '
+					<div class="flex-item">
+						<h3>'.$izbornik.'</h3>
 
-					<form action="index.php?menu=11" id="editnews" name="editnews" method="POST">
-						<input type="hidden" id="_editnews_" name="_editnews_" value="TRUE">
-						<input type="hidden" id="idvijesti" name="idvijesti" value="' . $r['id'] . '">
-						<input type="submit" value="Edit"><br>
-					</form>
+						<form action="index.php?menu=2" id="publishingtoggle" name="publishingtoggle" method="POST">
+							<input type="hidden" id="' . $identifikator . '" name="' . $identifikator . '" value="TRUE">
+							<input type="hidden" id="idvijesti" name="idvijesti" value="' . $r['id'] . '">
+							<input type="submit" value="' . $tekst . '"><br>
+						</form>
 
-					<form action="index.php?menu=2" id="deletenews" name="deletenews" method="POST">
-						<input type="hidden" id="_deletenews_" name="_deletenews_" value="TRUE">
-						<input type="hidden" id="idvijesti" name="idvijesti" value="' . $r['id'] . '">
-						<input type="submit" value="Delete"><br>
-					</form>
-				</div>
-			</div>
+						<form action="index.php?menu=11" id="editnews" name="editnews" method="POST">
+							<input type="hidden" id="_editnews_" name="_editnews_" value="TRUE">
+							<input type="hidden" id="idvijesti" name="idvijesti" value="' . $r['id'] . '">
+							<input type="submit" value="Edit"><br>
+						</form>
+				';
+
+						if ($_SESSION['user']['role'] == 1)	{
+							echo '
+							<form action="index.php?menu=2" id="deletenews" name="deletenews" method="POST">
+								<input type="hidden" id="_deletenews_" name="_deletenews_" value="TRUE">
+								<input type="hidden" id="idvijesti" name="idvijesti" value="' . $r['id'] . '">
+								<input type="submit" value="Delete"><br>
+							</form>
+							';
+						}
+						
+					echo '</div>';
+				}
+			echo '</div>
 		';
 	};
 	
